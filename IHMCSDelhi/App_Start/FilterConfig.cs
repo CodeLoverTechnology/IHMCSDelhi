e60c68013247081d42d13a49cfafb05b199fa660
RAHUL KUMAR <rahulinbscit@live.com>
2019-12-18 17:16:52 +0530

    Add project files.

---
+++ IHMCSDelhi/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace IHMCSDelhi
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ IHMCSDelhi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IHMCSDelhi.MvcApplication" Language="C#" %>
